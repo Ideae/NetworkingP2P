6bf483efbb82b6a5dd90863315ab6c361a38a25b
Ideae <zack.tibia@gmail.com>
2014-11-29 02:37:16 -0500

    refactored udp client logic

--- NetworkingP2P/src/com/company/P2PClient.java
+++ NetworkingP2P/src/com/company/P2PClient.java
@@ -15,12 +15,9 @@ public class P2PClient
 
     public static void main(String[] args) throws IOException
     {
-        //if (args.length != 1) {
-        //    System.out.println("Usage: java P2PClient <hostname>");
-        //    return;
-        //}
         serverRecords.put(1, new ServerRecord(1, "127.0.0.1", 4441));
-        MakeRequest("init", "init", 1);
+        //MakeRequest("init", "init", 1);
+        Init();
 
         Scanner sc = new Scanner(System.in);
         while(true)
@@ -31,22 +28,39 @@ public class P2PClient
             if (command.equals("update"))
             {
                 String filename = scLine.next();
-                int serverNum = Utils.Hash(filename);
-                MakeRequest(command, command + " " + filename, serverNum);
+                //int serverNum = Utils.Hash(filename);
+                //MakeRequest(command, command + " " + filename, serverNum);
+                Update(filename);
             }
 
         }
     }
-    static void MakeRequest(String command, String request, int serverNum) throws IOException
+    static void Update(String contentName) throws IOException
     {
-        //String hostname = "127.0.0.1";
-        // get a datagram socket
-        DatagramSocket socket = new DatagramSocket();
-
+        String request = "update " + contentName;
+        int serverNum = Utils.Hash(contentName);
+        String response = CreateRequest(request, serverNum);
+        contentToDHTServer.put(contentName, serverNum);
+        System.out.printf("Stored %s in server %d \n", contentName, serverNum);
+    }
+    static void Init() throws IOException
+    {
+        String response = CreateRequest("init", 1);
+        System.out.println("Response: " + response);
+        Scanner sc = new Scanner(response);
+        while (sc.hasNext())
+        {
+            int num = Integer.parseInt(sc.next());
+            String serverIP = sc.next();
+            int serverPort = Integer.parseInt(sc.next());
+            if (!serverRecords.containsKey(num))
+                serverRecords.put(num, new ServerRecord(num, serverIP, serverPort));
+        }
+    }
+    static String CreateRequest(String request, int serverNum) throws IOException
+    {
         ServerRecord record = serverRecords.get(serverNum);
-        //String message = "init";
-        // send request
-        //byte[] buf = new byte[256];
+        DatagramSocket socket = new DatagramSocket();
         byte[] buf = request.getBytes();
         InetAddress address = InetAddress.getByName(record.IPAddress);
         DatagramPacket packet = new DatagramPacket(buf, buf.length, address, record.portNumber);
@@ -59,27 +73,7 @@ public class P2PClient
 
         // display response
         String received = new String(packet.getData(), 0, packet.getLength());
-        System.out.println("Response: " + received);
-
-        if (command.equals("init"))
-        {
-            Scanner sc = new Scanner(received);
-            while (sc.hasNext())
-            {
-                int num = Integer.parseInt(sc.next());
-                String serverIP = sc.next();
-                int serverPort = Integer.parseInt(sc.next());
-                if (!serverRecords.containsKey(num))
-                    serverRecords.put(num, new ServerRecord(num, serverIP, serverPort));
-            }
-        }
-        else if (command.equals("update"))
-        {
-            Scanner sc = new Scanner(request); sc.next();
-            String contentName = sc.next();
-            contentToDHTServer.put(contentName, Utils.Hash(contentName));
-            System.out.printf("Stored %s in records\n", contentName);
-        }
         socket.close();
+        return received;
     }
 }
